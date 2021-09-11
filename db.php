7e3a0954d258560931e3f79a3b9d6c2aeac69f5c
Shauvik Paul <paulsayar99@gmail.com>
2021-09-12 03:44:38 +0530

    changes made to host

--- db.php
+++ db.php
@@ -1,6 +1,5 @@
 <?php
 
-$con = mysqli_connect('localhost','root','','phpassignment');
-
+$con = mysqli_connect('sql204.epizy.com','epiz_29703014','UAZ9AdqaHXpP43','epiz_29703014_phpassignment');
 
 ?>
